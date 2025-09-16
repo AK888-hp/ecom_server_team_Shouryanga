002d66bc059baeef0258674698eb4490859609eb
Your Name <you@example.com>
2025-09-16 16:07:49 +0530

    commit

---
+++ src/main/java/com/mtd/ecom_server/models/Product.java
@@ -0,0 +1,66 @@
+package com.mtd.ecom_server.models;
+
+import org.springframework.data.annotation.Id;
+
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+	@Id
+	private String id;
+	private String name;
+	private String description;
+	private String category;
+	private String tags;
+	private Float price;
+	private Integer stock;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getTags() {
+		return tags;
+	}
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	public Integer getStock() {
+		return stock;
+	}
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	
+	
+
+	
+
+}
